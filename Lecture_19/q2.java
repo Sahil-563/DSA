4aa5297e64c44851c6d23aaec82176f2601230ca
Sahil <sahil2002ece@gmail.com>
2023-05-15 20:47:54 +0545

    Stock Span problem

---
+++ Lecture_19/q2.java
@@ -0,0 +1,33 @@
+package Lecture_19;
+import java.util.*;
+public class q2 {
+    public static void stockSpan(int stock[],int span[]){
+        Stack<Integer> s = new Stack<>();
+        span[0] = 1;
+        s.push(0);
+        for(int i=1;i<stock.length;i++){
+            int curr = stock[i];
+            while(!s.isEmpty() && curr>stock[s.peek()]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                span[i]=i+1;
+            }
+            else{
+                int prevHigh = s.peek();
+                span[i]=i-prevHigh;
+            }
+            s.push(i);
+        }
+    }
+
+    public static void main(String args[]){
+        int stock[]={70,80,60,70,60,85,100};
+        int span[] = new int[stock.length];
+        stockSpan(stock,span);
+        for(int i=0;i<span.length;i++){
+            System.out.println(span[i]+" ");
+        }
+    }
+    
+}
